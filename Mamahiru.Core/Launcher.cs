11ecc1a2897883bc3a5d9c9a01c4715034f701a0
ddasutein <dasutein@outlook.com>
2021-01-05 08:59:46 +0800

    Add config management

--- Mamahiru.Core/Launcher.cs
+++ Mamahiru.Core/Launcher.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using Discord;
 using Discord.WebSocket;
+using Mamahiru.Core.Modules.BMS;
 
 namespace Mamahiru.Core
 {
@@ -14,17 +15,28 @@ namespace Mamahiru.Core
 
         public static async Task MainAsync()
         {
+            LoadConfig.LoadJSON();
             Console.WriteLine("DISCORD Bot name: Mamahiru");
             Console.WriteLine("VERSION: 0.0.1-alpha");
 
             _client = new DiscordSocketClient();
             _client.Log += Log;
 
+            if (LoadConfig.settings.token == null)
+            {
+                Console.WriteLine("token empty");
+                Console.ReadKey();
+                return;
+            }
 
-            string token = "";
+            string token = LoadConfig.settings.token;
             await _client.LoginAsync(TokenType.Bot, token);
             await _client.StartAsync();
             await Task.Delay(-1);
+
+            LoadConfig.LoadJSON();
+            LoadConfig.settings.token = "test";
+            LoadConfig.SaveJson();
 
         }
 
