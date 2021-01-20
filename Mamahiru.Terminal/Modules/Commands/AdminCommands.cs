f4b219ce020bfc8da30a939b8d0bd53e5ad1618b
ddasutein <dasutein@outlook.com>
2021-01-20 08:33:22 +0800

    Add command files

---
+++ Mamahiru.Terminal/Modules/Commands/AdminCommands.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mamahiru.Terminal.Modules.Commands
+{
+    class AdminCommands
+    {
+    }
+}
---
+++ Mamahiru.Terminal/Modules/Commands/UserCommands.cs
@@ -0,0 +1,23 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mamahiru.Terminal.Modules.Commands
+{
+    public class UserCommands : ModuleBase
+    {
+        [Command("hello")]
+        private async Task Hello()
+        {
+            var sb = new StringBuilder();
+            var user = Context.User;
+
+            sb.AppendLine($"Hi there, -> {user}");
+            sb.AppendLine("I am Mamahiru Bot! Yoroshiku Onegaishimasu :wave:");
+
+            await ReplyAsync(sb.ToString());
+        }
+    }
+}
